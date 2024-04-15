63ecdc0b63df6f6a34cd8442847b046182d636f8
Ash7gaut <antoningautier@outlook.fr>
2024-04-15 15:05:14 +0200

    feat(itinerary) added updatedAt

--- validators/ItineraryValidator.js
+++ validators/ItineraryValidator.js
@@ -4,6 +4,7 @@ const ItineraryValidator = zod.object({
   Prompt: zod.string().min(1).max(255),
   Answer: zod.string().min(1).max(900),
   CreatedAt: zod.string().datetime().optional(),
+  UpdatedAt: zod.string().datetime().optional(),
 });
 
 export default ItineraryValidator;
