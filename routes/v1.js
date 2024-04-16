774040d7f0f6d801ada048b61a0c42fa12f296c9
Ash7gaut <antoningautier@outlook.fr>
2024-04-16 15:26:14 +0200

    fix(mistral) mistral to itinerary

--- routes/v1.js
+++ routes/v1.js
@@ -3,9 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 import ItineraryRouter from './v1/itinerary.js';
-import MistralAPIRouter from './v1/mistral.js';
+// import MistralAPIRouter from './v1/mistral.js';
 
 router.use('/itinerary', ItineraryRouter);
-router.use('/mistral', MistralAPIRouter);
+// router.use('/mistral', MistralAPIRouter);
 
 export default router;
--- routes/v1/itinerary.js
+++ routes/v1/itinerary.js
@@ -1,44 +1,56 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 import ItineraryValidator from '../../validators/ItineraryValidator.js';
 
+dotenv.config();
+
 const router = express.Router();
-
 const prisma = new PrismaClient();
-
-router.get('/', async (req, res) => {
-  const itineraries = await prisma.itinerary.findMany();
-  res.json(itineraries);
-});
-
-router.get('/:id', async (req, res) => {
-
-  const { id } = req.params;
-  const itinerary = await prisma.itinerary.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
-  if (itinerary) {
-    res.json(itinerary);
-  }
-});
-
+const MISTRAL_API_KEY = process.env.MISTRAL_API_KEY;
+const prePrompt = "Tu es un planificateur de voyage, expert en tourisme. Pour la destination, le nombre de jours et le moyen de locomotion que je te donnerai à la fin du message, programme moi un itinéraire en plusieurs étapes Format de données souhaité: une liste d'élement en JSON Avec, pour chaque étape: - le nom du lieu (clef JSON: name) -sa position géographique (clef JSON: location-> avec latitude/longitude en numérique) - une courte description du lieu (clef JSON: description) Donne-moi juste cette liste d'étape, sans texte autour. Il ne faut vraiment AUCUN texte autour en dehors du JSON, il faut impérativement que le JSON soit bien formé et valide sans rien autour sinon le modèle ne pourra pas le lire.";
 
 router.post("/", async (req, res) => {
-  const { prompt, answer, createdAt, updatedAt } =
-    ItineraryValidator.parse(req.body);
+  const { prompt } = req.body;
 
-  const itinerary = await prisma.itinerary.create({
-    data: {
-      prompt,
-      answer,
-      createdAt,
-      updatedAt,
-    },
-  });
+  if (!prompt) {
+    return res.status(400).json({ error: 'The field "prompt" is required.' });
+  }
 
-  res.status(201).json(itinerary);
+  try {
+    const mistralResponse = await fetch(
+      "https://api.mistral.ai/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${MISTRAL_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "open-mistral-7b",
+          messages: [{ role: "user", content: prePrompt + " " + prompt }],
+        }),
+      }
+    );
+
+    const mistralData = await mistralResponse.json();
+
+    console.log(mistralData.choices[0].message.content);
+    // const { answer, createdAt, updatedAt } = ItineraryValidator.parse(req.body);
+
+    const itinerary = await prisma.itinerary.create({
+      data: {
+        prompt,
+        answer: JSON.parse(mistralData.choices[0].message.content)
+      },
+    });
+
+    res.status(201).json(itinerary);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({});
+  }
 });
 
 router.patch("/:id", async (req, res) => {
