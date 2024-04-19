2f08e53619ce9f987e6efcea9fbe8373fb114b83
Ash7gaut <antoningautier@outlook.fr>
2024-04-19 13:58:21 +0200

    fix: hostname

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log('Server is running on port 3000');
 });
